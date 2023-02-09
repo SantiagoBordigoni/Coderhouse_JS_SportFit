b04086dc0c399b8324367d2a01c025bb35daa029
SantiagoBordigoni <santibordigonidiaz@gmail.com>
2023-02-09 20:56:31 -0300

    Se agrego el carrito de compras

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // SELECCIONADOR DE PLANES DE GIMNASIO
+// EN ESTE CICLO OBTENEMOS INFORMACION DE CADA PLAN SI ASI LO DESEARAMOS
 let informacion = parseInt(prompt("Seleccione del 1 al 3 para obtener informacion acerca de cada plan. Escriba 0 para salir"))
 while (informacion != 0){
     if (informacion==1){
@@ -17,7 +18,7 @@ while (informacion != 0){
     }
 
 }
-
+// SELECCIONAMOS LA CANTIDAD DE PERSONAS QUE DESEAMOS ASOCIAR Y EN CADA CICLO ELEGIMOS EL PLAN PARA LA PERSONA
 let cantidad = parseInt(prompt("Ingrese la cantidad de personas que desea asociar"))
 let precioTotal = 0
 for (let i=1; i<= cantidad; i++){
@@ -40,7 +41,9 @@ for (let i=1; i<= cantidad; i++){
     }
 
     precioTotal += precioPlanMes
+    // SE VAN SUMANDO LOS MONTOS DE CADA PLAN SELECCIONADO
 }
+// EN ESTE CICLO LLEGADO EL CASO QUE ASOCIEMOS A MAS DE 2 PERSONAS, TENEMOS DESCUENTO POR CANTIDAD
 if (cantidad>=3){
     alert("Gracias! Por la compra de 3 o más planes se te otorgó un descuento del 15%")
     let descuentoXCantidad = 0.15 * precioTotal
@@ -49,7 +52,69 @@ if (cantidad>=3){
 }
 console.log("El precio total a pagar por los " + cantidad + " planes es de: $" +precioTotal)
 
+// SE INCLUYE CARRITO DE COMPRAS DE ARTICULOS DE GYM ADEMAS DE LA MEMBRESIA PARA ACCEDER AL GIMNASIO
 
 
 
+// ACA TENEMOS EL ARRAY CON LOS DISTINTOS OBJETOS, QUE SERIAN LOS ARTICULOS DE GIMNASIO CON SUS RESPECTIVOS ATRIBUTOS
+const equipoGym = [
+    {
+      nombre: "Pesas de 5 kg",
+      precio: 2500,
+      stock: 10
+    },
+    {
+      nombre: "Pesas de 10 kg",
+      precio: 4000,
+      stock: 5
+    },
+    {
+      nombre: "Barra de levantamiento de pesas",
+      precio: 7000,
+      stock: 2
+    },
+    {
+      nombre: "Mancuernas de 25 kg",
+      precio: 15000,
+      stock: 3
+    },
+    {
+      nombre: "Rack de entrenamiento",
+      precio: 60000,
+      stock: 1
+    }
+  ];
 
+//   EN PRINCIPIO NO TENEMOS ARTICULOS SELECCIONADOS, PERO DEFINIMOS LA LISTA PORQUE AL SELECCIONAR SE VAN A IR CARGANDO AHI
+let equipoSeleccionado = [];
+//   LA FUNCION AGREGAR AL CARRITO TOMA EL PARAMETRO QUE DA EL USUARIO, LA ENTRADA Y BUSCA ESE ARTICULO. 
+function agregarAlCarrito(nombreEquipo) {
+    const equipo = equipoGym.find( item => item.nombre.toLowerCase() === nombreEquipo.toLowerCase());
+    if (!equipo) {
+      console.log("Artículo no encontrado");
+      return;
+    }
+    if (equipo.stock === 0) {
+      console.log("Artículo sin stock");
+      return;
+    }
+    equipoSeleccionado.push(equipo);
+    equipo.stock -= 1;
+    console.log(`Artículo "${equipo.nombre}" agregado al carrito`);
+    // EN CASO DE EXISTIR Y HABER STOCK, SE AGREGA A LA LISTA DE EQUIPOS SELECCIONADOS Y SE REDUCE EN UNA UNIDAD EL STOCK DE LA LISTA DE EQUIPO DISPONIBLE
+  }
+  
+function calcularTotal() {
+    let total = 0;
+    for (const equipo of equipoSeleccionado) {
+        total += equipo.precio;
+    }
+    console.log(`Total: $${total}`);
+  }
+
+let articulo =prompt("Ingrese el articulo que desea comprar o 'salir':");
+while (articulo!="salir"){
+    agregarAlCarrito(articulo)
+    articulo= prompt("Ingrese el articulo que desea comprar o 'salir':");}
+
+calcularTotal();
